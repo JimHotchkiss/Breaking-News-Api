e7349d0e93f2d9dfe05889676bcc8cd1b54a47f9
github username <github email address>
2019-09-15 19:11:26 -0700

    Added time function and title.

--- src/js/news.js
+++ src/js/news.js
@@ -4,6 +4,7 @@ $(() => {
     newsTitle();
     newsDescription();
     publishedAt();
+    time();
   });
 
 getNews = () => {
@@ -13,6 +14,8 @@ getNews = () => {
     fetch('https://newsapi.org/v2///top-headlines?' + 'country=us&' + 'apiKey=bcd9264c4d4646b5a22d288a9a796d3d')
     .then(response => response.json())
     .then(news => {
+      let newsDiv = document.getElementById('newsDiv')
+      newsDiv.style.display = 'none'
         showNews(news);
     })
 })
@@ -41,24 +44,47 @@ getNews = () => {
   }
 
   newsDescription = (story) => {
-      console.log(story.publishedAt)
+      //console.log(story.publishedAt)
       // Set Title
       let titleDiv = document.createElement('div');
       let titleP = document.createElement('p');
+      let titleH = document.createElement('h5');
+      titleH.setAttribute('class', 'title-h');
+      titleH.appendChild(document.createTextNode(story.title));
       // Insert text
-      titleP.appendChild
-      (document.createTextNode(story.description));
+      titleP.appendChild(document.createTextNode(story.description));
       // Insert P tag into div
+      titleDiv.appendChild(titleH)
       titleDiv.appendChild(titleP);
       ulNews.appendChild(titleDiv);
       publishedAt(story);
   }
 
   publishedAt = (story) => {
+    let currentDate = new Date();
     let date = new Date(story.publishedAt);
-    date.toString() 
-    console.log(date)
-
+    let milliSeconds = currentDate.getTime() - date.getTime();
+    let hours = Math.floor(milliSeconds / 3600000) 
+    let timeDiv = document.createElement('div');
+    let timeP = document.createElement('p');
+    let timeStatement = document.createElement('p');
+    timeStatement.setAttribute('class', 'time-statement');
+    timeStatement.appendChild(document.createTextNode(" This story broke"))
+    timeP.setAttribute('class', 'time-p');
+    //timeP.appendChild(document.createTextNode(hours));
+      if (milliSeconds < 3600000) {
+        timeP.appendChild(document.createTextNode(' '));
+        timeP.appendChild(document.createTextNode(" less than an hour ago"));
+      } else if (hours === 1){
+        timeP.appendChild(document.createTextNode(hours));
+        timeP.appendChild(document.createTextNode(" hour ago"));
+      } else {
+        timeP.appendChild(document.createTextNode(hours));
+        timeP.appendChild(document.createTextNode(" hours ago"));
+      }
+    timeDiv.appendChild(timeStatement)
+    timeDiv.appendChild(timeP);
+    ulNews.appendChild(timeDiv);
   }
 }
 
